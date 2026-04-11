0aa9be63534c68163c47aafb270e1e33254af4ef
Kiran Danduprolu <kd@danduprolu.com>
2026-04-12 01:41:14 +0530

    chore: remove all LiveView modules, HEEX layouts, and JS hooks
    Delete lib/moth_web/live/, ui.ex, game.ex, layouts.ex, layout HEEX
    templates, assets/js/hooks/, and assets/js/app.js. Strip LiveView and
    layout references from moth_web.ex and router.ex. Fix error_html.ex to
    use Phoenix.Component directly.

--- lib/moth_web/components/layouts.ex
+++
@@ -1,8 +0,0 @@
-defmodule MothWeb.Layouts do
-  @moduledoc """
-  Layout components for MothWeb.
-  """
-  use MothWeb, :html
-
-  embed_templates "layouts/*"
-end
--- lib/moth_web/router.ex
+++ lib/moth_web/router.ex
@@ -4,8 +4,6 @@ defmodule MothWeb.Router do
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
-    plug :fetch_live_flash
-    plug :put_root_layout, html: {MothWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
     plug MothWeb.Plugs.Auth
