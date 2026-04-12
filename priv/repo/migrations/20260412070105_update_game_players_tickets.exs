4b40be8df0d81555200a5542da2c5570f3d73299
Kiran Danduprolu <kd@danduprolu.com>
2026-04-12 12:31:26 +0530

    feat: migrate game_players.ticket -> tickets array

---
+++ priv/repo/migrations/20260412070105_update_game_players_tickets.exs
@@ -0,0 +1,10 @@
+defmodule Moth.Repo.Migrations.UpdateGamePlayersTickets do
+  use Ecto.Migration
+
+  def change do
+    alter table(:game_players) do
+      remove :ticket
+      add :tickets, {:array, :map}, default: []
+    end
+  end
+end
